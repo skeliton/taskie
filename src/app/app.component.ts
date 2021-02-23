ee33e512da2215cdafffd06c20bef7d6779d754c
Dan Stewart <dannstewart@outlook.com>
2021-02-23 16:45:12 -0600

    initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'taskie';
+  title = 'Taskie';
+  taskieFilterSelected = 'all';
+  taskPanelOpenState = true;
+  focusedTaskName = "Create Dashboard Page"
+  displayedColumns = ['position', 'name', 'type', 'status'];
+  dataSource = TASK_DATA;
 }
+
+export interface TaskieTask {
+  name: string;
+  position: number;
+  type: string;
+  status: string;
+}
+
+const TASK_DATA: TaskieTask[] = [
+  {position: 1, name: 'Create Dashboard Page', type: 'Personal', status: 'InProcess'},
+  {position: 2, name: 'Create Focused Task Utility', type: 'Personal', status: 'InProcess'},
+  {position: 3, name: 'Create Task Grid', type: 'Personal', status: 'InProcess'},
+  {position: 3, name: 'Create Backlog Grid', type: 'Personal', status: 'InProcess'},
+];
